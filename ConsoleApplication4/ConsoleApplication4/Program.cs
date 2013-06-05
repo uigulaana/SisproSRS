23b72afecdc275d83b84234a1c8a936de4442f7e
RuslanPakhomov <Chessgross93@mail.ru>
2013-06-05 22:19:11 +1000

    prostoe 4uslo

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static bool Prostoe(int number)
+        {
+            if (number < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            int s = 0;
+            int i = 0;
+            int otvet = 0;
+            Console.WriteLine("Введите простое число");
+            n = Convert.ToInt16(Console.ReadLine());
+            s = n;
+            s=s+1;
+            for (i = s; i < 10000; i++)
+            {
+                if (Prostoe(i))
+                {
+                    otvet = i;
+                    break;
+                }
+            }
+            Console.WriteLine(otvet);
+         }
+    }
+}
