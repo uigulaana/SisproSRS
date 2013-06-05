7aea94608d4824e099a8d47df46e45925b32dd2c
uigulaana <gavrilieva93@mail.com>
2013-06-05 19:17:20 +1000

    3adomNapered

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int[] massiv = { 1, 2, 3, 4, 5, 6, 7 };
+            for (i = massiv.Length - 1; i >= 0; i--)
+            {
+                Console.Write(" " + massiv[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
