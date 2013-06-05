181e963ee426340e539fdf6f5a4886957bce125b
phyzicx <phyzicx@mail.ru>
2013-06-05 20:58:06 +1000

    integral

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, y, Integral;
+            double sum = 0;
+            double h = 0.000001;
+            for (a = -1; a <= 1; a = a + h)
+            {
+                y = Math.Sqrt(1 - a * a);
+                sum = sum + y;
+            }
+            Integral = h * sum;
+            Console.WriteLine(Integral);
+        }
+    }
+}
